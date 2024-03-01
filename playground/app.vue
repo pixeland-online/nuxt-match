f440f81722fae6d6700044b9bb490e97b15a59f0
JCtapuk <61321608+JCtapuk@users.noreply.github.com>
2024-03-01 19:50:43 +0000

    upd

--- playground/app.vue
+++ playground/app.vue
@@ -5,14 +5,21 @@
 <script setup lang="ts">
 let ws: WebSocket | undefined;
 
-onMounted(() => {
-  const isSecure = location.protocol === "https:";
-  const url = (isSecure ? "wss://" : "ws://") + location.host + "/match";
-
+// Test Connect WebSocket
+async function connect() {
   if (ws) {
     ws.close();
   }
 
+  const isSecure = location.protocol === "https:";
+  const url = (isSecure ? "wss://" : "ws://") + location.host + "/match";
+
   ws = new WebSocket(url);
+
+  await new Promise((resolve) => ws?.addEventListener('open', resolve))
+}
+
+onMounted(() => {
+  connect()
 });
 </script>
